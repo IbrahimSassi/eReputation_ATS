d8f9e8a94f4fc7d95b1d14b8e64096af60c805d9
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-01-29 19:07:50 +0100

    fixing template + clean up

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
-import * as jQuery from 'jquery';
 
 if (environment.production) {
   enableProdMode();
 }
 
-jQuery(() => platformBrowserDynamic().bootstrapModule(AppModule));
-
+platformBrowserDynamic().bootstrapModule(AppModule);
