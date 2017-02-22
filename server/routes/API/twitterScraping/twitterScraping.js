5fb25479b1d1b7fe88ecc6538ed7a3f2887fbeac
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-02-22 14:23:44 +0100

    Working on getting tweets based on username

--- server/routes/API/twitterScraping/twitterScraping.js
+++ server/routes/API/twitterScraping/twitterScraping.js
@@ -45,26 +45,84 @@ router.get('/streaming', function(req, res) {
     });
   });
 
+});
 
 
-  router.get('/about',function (req,res) {
 
-    client.get('search/tweets', {q: 'Mohamed Firas Ouertani'}, function(error, tweets, response) {
 
-      //JSON
-      res.json(tweets);
-      //console.log("This is it: ",tweets);
+router.get('/about',function (req,res) {
 
-    });
+  client.get('search/tweets', {q: 'Mohamed Firas Ouertani'}, function(error, tweets, response) {
 
+    //JSON
+    res.json(tweets);
+    //console.log("This is it: ",tweets);
 
   });
 
 
-
-
 });
 
+
+
+
+
+router.get('/user_timeline/:user', function(req, res) {
+/*
+  var oEmbedTweets = [], tweets = [],
+
+    params = {
+      screen_name: req.params.user, // the user id passed in as part of the route
+      count: TWEET_COUNT // how many tweets to return
+    };
+
+  // the max_id is passed in via a query string param
+  if(req.query.max_id) {
+    params.max_id = req.query.max_id;
+  }
+*/
+
+  // request data
+  twitter.get(USER_TIMELINE_URL, function (err, data, resp) {
+
+   // tweets = data;
+    //JSON
+    res.json(data);
+    /*var i = 0, len = tweets.length;
+
+    for(i; i < len; i++) {
+      getOEmbed(tweets[i]);
+    }*/
+  });
+
+  /**
+   * requests the oEmbed html
+   */
+/*
+  function getOEmbed (tweet) {
+
+    // oEmbed request params
+    var params = {
+      "id": tweet.id_str,
+      "maxwidth": MAX_WIDTH,
+      "hide_thread": true,
+      "omit_script": true
+    };
+
+    // request data
+    twitter.get(OEMBED_URL, params, function (err, data, resp) {
+      tweet.oEmbed = data;
+      oEmbedTweets.push(tweet);
+
+      // do we have oEmbed HTML for all Tweets?
+      if (oEmbedTweets.length == tweets.length) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(oEmbedTweets);
+      }
+    });
+  }
+  */
+});
 
 
 
