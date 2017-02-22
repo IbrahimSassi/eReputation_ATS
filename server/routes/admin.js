eda2a5a2fa8959d16017d20e21fe75f6ab34607d
HakimMliki <hakim.mliki@esprit.tn>
2017-02-22 13:53:51 +0100

    fixing bug

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Express' });
+  res.render('admin', {  });
 });
 
 module.exports = router;
