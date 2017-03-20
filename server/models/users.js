90883d0be40bdae572f1a873c933886cb48c4be4
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-03-20 16:21:39 +0100

    Working with user controller, service on front end, and user routers fro back end

--- server/models/users.js
+++ server/models/users.js
@@ -3,8 +3,10 @@
  */
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-var UsersSchema   = new Schema({
+var UserSchema   = new Schema({
   username: String,
   firstName: String,
   lastName: String,
@@ -26,4 +28,27 @@ var UsersSchema   = new Schema({
   creationDate:String
 });
 
-module.exports = mongoose.model('Users', UsersSchema);
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hashedPassword = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hashedPassword = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hashedPassword === hashedPassword;
+};
+
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+module.exports = mongoose.model('User', UserSchema);
--- server/public/angular/app/user/user.service.js
+++ server/public/angular/app/user/user.service.js
@@ -8,10 +8,10 @@
     .module('ATSApp-front.user')
     .service('UserService', UserServiceFN);
 
-  UserServiceFN.$inject = ['UserFactory'];
+  UserServiceFN.$inject = ['UserFactory','$http', '$window'];
 
 
-  function UserServiceFN(UserFactory) {
+  function UserServiceFN(UserFactory,$http, $window) {
 
 
     this.getAllUsers = function () {
@@ -19,6 +19,71 @@
       return UserFactory.query().$promise;
 
     }
+
+    var saveToken = function (token) {
+      $window.localStorage['mean-token'] = token;
+    };
+
+    var getToken = function () {
+      return $window.localStorage['mean-token'];
+    };
+
+    var isLoggedIn = function() {
+      var token = getToken();
+      var payload;
+
+      if(token){
+        payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    var currentUser = function() {
+      if(isLoggedIn()){
+        var token = getToken();
+        var payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+        return {
+          email : payload.email,
+          name : payload.name
+        };
+      }
+    };
+
+     var register = function(user) {
+      return $http.post('/users/register', user).then(function(data,err){
+        saveToken(data.token);
+      });
+    };
+
+     var login = function(user) {
+      return $http.post('/users/login', user).success(function(data) {
+        saveToken(data.token);
+      });
+    };
+
+    var logout = function() {
+      $window.localStorage.removeItem('mean-token');
+    };
+
+    return {
+      currentUser : currentUser,
+      saveToken : saveToken,
+      getToken : getToken,
+      isLoggedIn : isLoggedIn,
+      register : register,
+      login : login,
+      logout : logout
+    };
+
+
+
 
 
   }
