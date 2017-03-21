e90dbed5337b85174eb11fa8c9ba742617b99416
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-03-21 16:30:26 +0100

    Start Working On Form Validation

--- server/public/angular/app/channel/controllers/create-channel.controller.js
+++ server/public/angular/app/channel/controllers/create-channel.controller.js
@@ -36,8 +36,10 @@
       type: "",
       accessToken: "",
       personnel: false,
-      userId : vm.connectedUserId
+      userId: vm.connectedUserId
     };
+
+    vm.myFacebookPages = [];
 
     init();
 
@@ -46,27 +48,35 @@
     }
 
 
-    vm.createChannel = function () {
+    vm.createChannel = function (form) {
       ChannelService.addChannel(vm.channel)
         .then(function (result) {
-        console.log("result", result);
-        $state.go('channels');
+          console.log("result", result);
+          $state.go('channels');
+          var $toastContent = $('<span class="green-text">New Channel has just created</span>');
+          var rounded = "rounded"
+          Materialize.toast($toastContent, 3000, rounded);
 
-      });
+
+
+        });
     };
 
     vm.getPermissions = function () {
       FacebookService.initFacebookApi()
         .then(function (data) {
-        console.log("here we are token  + user ,,promise bouh kalb",data);
-        var token = data.authResponse.accessToken;
+          console.log("here we are token  + user ,,promise bouh kalb", data);
+          var token = data.authResponse.accessToken;
 
-        FacebookService.getLongLivedToken(token).then(function (newLongToken) {
-          console.log(newLongToken);
-          vm.channel.accessToken =newLongToken.longToken;
-        })
+          FacebookService.getLongLivedToken(token).then(function (newLongToken) {
+            console.log(newLongToken);
+            vm.channel.accessToken = newLongToken.longToken;
+            data.user.accounts.data.forEach(function (page) {
+              vm.myFacebookPages.push({value: page.id, text: page.name})
+            });
+          })
 
-      });
+        });
     };
 
 
