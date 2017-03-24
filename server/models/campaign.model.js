a173b755e8f211a9ffe145808933d71952465db2
Narimen Hadjkacem <narimen.hadjkacem@esprit.tn>
2017-03-24 21:38:26 +0100

    add charts

--- server/models/campaign.model.js
+++ server/models/campaign.model.js
@@ -6,7 +6,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-/**Animal Schema*/
+/**Campaign Schema*/
+
+/*object Campaign example : http://www.jsoneditoronline.org/?id=23467b85d3d9b5b5b5a04cb21f0daea5 */
 
 var campaignSchema = new Schema({
   name: {
@@ -44,15 +46,11 @@ var campaignSchema = new Schema({
         type: Schema.Types.ObjectId,
         required: true,
       },
-      _id:false,
+      _id: false,
     }
   ],
   keywords: [
     {
-      _id: {
-        type: Schema.Types.ObjectId,
-        default: new mongoose.Types.ObjectId,
-      },
       content: {
         type: String,
         required: true,
@@ -64,6 +62,11 @@ var campaignSchema = new Schema({
       creationDate: {
         type: Date,
         default: Date.now
+      },
+      state:{
+        type: String,
+        enum:['active','inactive'],
+        default:'active'
       }
     }
   ],
@@ -84,7 +87,7 @@ var campaignSchema = new Schema({
 var myCampaign = module.exports = mongoose.model('campaigns', campaignSchema);
 
 /**---------------------CRUD METHODS-----------------------**/
-// find All Animals
+// find All Campaign
 module.exports.findAllCampaigns = function () {
   return new Promise(function (resolve, reject) {
     myCampaign.find({}, function (err, docs) {
@@ -96,7 +99,7 @@ module.exports.findAllCampaigns = function () {
   });
 };
 
-// find Animal By ID
+// find Campaign By ID
 module.exports.findCampaignById = function (id) {
   return new Promise(function (resolve, reject) {
     myCampaign.find({_id: id}, function (err, docs) {
@@ -108,7 +111,7 @@ module.exports.findCampaignById = function (id) {
   });
 };
 
-// add an Animal
+// add an Campaign
 module.exports.saveCampaign = function (campaignToSave) {
   return new Promise(function (resolve, reject) {
     campaignToSave.save(function (err) {
@@ -123,7 +126,7 @@ module.exports.saveCampaign = function (campaignToSave) {
   });
 };
 
-// Update Animal
+// Update Campaign
 module.exports.updateCampaign = function (id, updatedAnimal) {
   return new Promise(function (resolve, reject) {
     myCampaign.findByIdAndUpdate(id, {$set: updatedAnimal}, {new: true}, function (err, docs) {
@@ -135,7 +138,7 @@ module.exports.updateCampaign = function (id, updatedAnimal) {
   });
 };
 
-// Delete Animal By Id
+// Delete Campaign By Id
 module.exports.removeCampaign = function (id) {
   return new Promise(function (resolve, reject) {
     myCampaign.remove({_id: id}, function (err) {
@@ -146,5 +149,98 @@ module.exports.removeCampaign = function (id) {
         resolve(data);
       });
     })
+  });
+};
+
+
+/**---------------------CRUD METHODS Keywords-----------------------**/
+
+//find all keywords from campaign
+
+module.exports.findAllKeywordsFromCampaign = function (campaignId) {
+  return new Promise(function (resolve, reject) {
+    myCampaign.find({_id: campaignId}, function (err, docs) {
+      if (err) {
+        reject(err);
+      }
+      resolve(docs);
+    }).select({"keywords": 1, "_id": 0});
+  });
+};
+
+//find all keywords by id from campaign
+
+module.exports.findKeywordsByIdFromCampaign = function (campaignId, keywordId) {
+  return new Promise(function (resolve, reject) {
+    myCampaign.find({_id: campaignId, keywords: {$elemMatch: {_id: keywordId}}}, function (err, docs) {
+      if (err) {
+        reject(err);
+      }
+      resolve(docs);
+    }).select({"keywords.$": 1, "_id": 0});
+  });
+};
+
+//remove keyword by id from campaign
+
+module.exports.removeKeywordsByIdFromCampaign = function (campaignId, keywordId) {
+  return new Promise(function (resolve, reject) {
+    myCampaign.findByIdAndUpdate(campaignId, {$pull: {keywords: {_id: keywordId}}}, function (err, docs) {
+      if (err) {
+        reject(err);
+      }
+      myCampaign.findAllKeywordsFromCampaign(campaignId).then(function (data) {
+        resolve(data);
+      });
+    });
+  });
+};
+
+//add keyword in campaign
+
+module.exports.addKeywordsByIdFromCampaign = function (campaignId, keyword) {
+  return new Promise(function (resolve, reject) {
+    myCampaign.findByIdAndUpdate(campaignId, {$push: {keywords: keyword}}, function (err, docs) {
+      if (err) {
+        reject(err);
+      }
+      myCampaign.findAllKeywordsFromCampaign(campaignId).then(function (data) {
+        resolve(data);
+      });
+    });
+  });
+};
+
+// update keyword in campaign
+module.exports.updateKeywordsByIdFromCampaign = function (campaignId, keywordId, updatedKeyword) {
+  return new Promise(function (resolve, reject) {
+
+    myCampaign.find({_id: campaignId, "keywords._id": keywordId}, function (err, docs) {
+
+      if (err) {
+        reject(err);
+      }
+
+      myCampaign.update({_id: campaignId, "keywords._id": keywordId}, {
+
+        $set: {
+
+          "keywords.$.content": updatedKeyword.content !== undefined ? updatedKeyword.content : docs[0].keywords[0].content,
+          "keywords.$.importance": updatedKeyword.importance !== undefined ? updatedKeyword.importance : docs[0].keywords[0].importance,
+          "keywords.$.state": updatedKeyword.state !== undefined ? updatedKeyword.state : docs[0].keywords[0].state,
+        }
+      }, function (err, docs) {
+        if (err) {
+          reject(err);
+        }
+        myCampaign.findAllKeywordsFromCampaign(campaignId).then(function (data) {
+          resolve(data);
+        });
+      });
+
+
+    }).select({"keywords.$": 1, "_id": 0});
+
+
   });
 };
--- server/routes/API/WWSA/index.js
+++ server/routes/API/WWSA/index.js
@@ -1,13 +1,7 @@
-/**
- * Created by HP on 11/03/2017.
- *********************** Word World Sentiment Analysis*******************
- */
-
 var express = require('express');
 var router = express.Router();
 var myRequest = require('request');
 var translate = require('google-translate-api');
-
 
 
 /* POST TEXT SEARCH . */
@@ -32,8 +26,20 @@ router.post('/', function (req, result, next) {
         console.log(error);
       } else {
         console.log(response.statusCode, body);
+
+        console.log( JSON.parse(body)[0].sentiment);
         //RESPONSE
-        result.json(JSON.parse(body));
+      //  result.json(JSON.parse(body));
+
+        /*******here*****/
+        var cleanResult = JSON.parse(body)[0].sentiment;
+
+
+         result.json({
+           positivity:(cleanResult.positive)*100,
+           negativity:(cleanResult.negative)*100,
+           neutrality:(cleanResult.neutral)*100,
+         });
 
       }
     });
@@ -44,5 +50,107 @@ router.post('/', function (req, result, next) {
 
 
 });
+
+
+
+router.post('/topic', function (req, result, next) {
+   var tab = [];
+  translate(req.body.text, {to: 'en'}).then(function (res) {
+    // console.log(res.text);
+    // console.log(res.from.language.iso);
+    myRequest({
+      url: 'http://apidemo.theysay.io/api/v1/topic',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: '{"text": "' + res.text + '", "level": "sentence"}'
+    }, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response.statusCode, body);
+
+        console.log( JSON.parse(body));
+
+
+        var x =JSON.parse(body)[0].scores.length ;
+        //RESPONSE
+       for (var i =0 ; i< x ; i++)
+        {
+          console.log(i)
+         tab.push({
+           topic : (JSON.parse(body)[0].scores[i].label),
+           confidence : (JSON.parse(body)[0].scores[i].confidence)
+         });
+         console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh/n", JSON.parse(body)[0].scores[i].label);
+        }
+         result.json(tab);
+
+      }
+    });
+
+  }).catch(function (err) {
+    console.error(err);
+  });
+
+
+});
+
+
+
+
+
+
+
+router.post('/emotions', function (req, result, next) {
+  var tab = [];
+  translate(req.body.text, {to: 'en'}).then(function (res) {
+    // console.log(res.text);
+    // console.log(res.from.language.iso);
+    myRequest({
+      url: 'http://apidemo.theysay.io/api/v1/emotion',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: '{"text": "' + res.text + '", "level": "sentence"}'
+    }, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response.statusCode, body);
+
+        console.log( JSON.parse(body));
+
+
+        var x =JSON.parse(body)[0].emotions.length ;
+        console.log(x)
+        //RESPONSE
+        for (var i =0 ; i< x ; i++)
+        {
+          console.log(i)
+          tab.push({
+            dimension : (JSON.parse(body)[0].emotions[i].dimension),
+            score : (JSON.parse(body)[0].emotions[i].score)
+          });
+          console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh/n", JSON.parse(body)[0].emotions[i].dimension);
+        }
+        result.json(tab);
+
+      }
+    });
+
+  }).catch(function (err) {
+    console.error(err);
+  });
+});
+
+
+
+
+
+
+
 
 module.exports = router;
