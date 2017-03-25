13ac196bcc30e2dc91eb910cf0e1cab898de5ab8
HakimMliki <hakim.mliki@esprit.tn>
2017-03-25 16:20:56 +0100

    insights Test

--- server/public/angular/app/campaign/campaign.controller.js
+++ server/public/angular/app/campaign/campaign.controller.js
@@ -59,22 +59,15 @@
 
 
     /** Scripts Loading first Refresh **/
-    // angularLoad.loadScript('angular/app/assets/js/charts/highcharts.js').then(function () {
-    //   angularLoad.loadScript('angular/app/assets/js/charts/exporting.js').then(function () {
-    //     angularLoad.loadScript('angular/app/assets/js/charts/column.js').then(function () {
-    //
-    //       console.log('all /dashboard scripts loaded OK');
-    //     })
-    //       .catch(function () {
-    //         console.log('err script 3');
-    //       });
-    //   })
-    //     .catch(function () {
-    //       console.log('err script 2');
-    //     });
-    // }).catch(function () {
-    //   console.log('err script 1');
-    // });
+    angularLoad.loadScript('angular/app/assets/js/charts/ggleloader.js').then(function () {
+      angularLoad.loadScript('angular/app/assets/js/charts/chartTest.js').then(function () {
+
+      }).catch(function () {
+        console.log('err script 1');
+      });
+    }).catch(function () {
+      console.log('err script 1');
+    });
     /** END of Scripts Loading first Refresh **/
 
   };
