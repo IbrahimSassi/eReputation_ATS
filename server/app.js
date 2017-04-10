2bbf4f31ddeb85acbd29c5399d3b3046319e43f4
HakimMliki <hakim.mliki@esprit.tn>
2017-04-10 16:12:12 +0100

    timeout server connection

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.engine('html', ejs.renderFile);
 //MongoDB Connection
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://bro:brobro0055@ds157469.mlab.com:57469/ats-digital',{
-  server : { autoReconnect : false }
+  server: { socketOptions: { connectTimeoutMS: 9879978979 }}
 });
  //mongoose.connect('mongodb://localhost:27017/ats-digital-local');
 
