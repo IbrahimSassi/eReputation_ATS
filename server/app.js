10427556df146488ff41f270756c3a61493347f5
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-04-10 01:37:23 +0100

    Fixing database host

--- server/app.js
+++ server/app.js
@@ -39,10 +39,10 @@ app.engine('html', ejs.renderFile);
 
 //MongoDB Connection
 var mongoose   = require('mongoose');
-/*mongoose.connect('mongodb://bro:brobro0055@ds157469.mlab.com:57469/ats-digital',{
+mongoose.connect('mongodb://bro:brobro0055@ds157469.mlab.com:57469/ats-digital',{
   server : { autoReconnect : false }
-});*/
- mongoose.connect('mongodb://localhost:27017/ats-digital-local');
+});
+ //mongoose.connect('mongodb://localhost:27017/ats-digital-local');
 
 //Adding passport require
 require('./config/passport');
