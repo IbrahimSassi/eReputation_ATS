d9b0501380cb33a47b157c688ef6882f3c392c92
MrFirases <mohamedfiras.ouertani@esprit.tn>
2017-04-26 02:08:32 +0100

    Updating twitter cron, fixing bugs

---
+++ server/routes/API/twitter/twitterAPIFunctions.js
@@ -0,0 +1,334 @@
+/**
+ * Created by MrFirases on 4/26/2017.
+ */
+
+var moment = require('moment');
+
+var config = require('../twitter/config');
+var TwitterStream = require('twitter');
+var dataProvider = require('../../../models/dataProvider/dataProvider.model');
+var async = require('async');
+var translate = require('google-translate-api');
+var utils = require('../helpers/utils.helper');
+
+var client = new TwitterStream({
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  access_token_key: config.twitter.access_token,
+  access_token_secret: config.twitter.access_token_secret
+});
+
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+
+
+module.exports = {
+  SaveDatToTwitterProviderForRepliesToUserForChannel: SaveDatToTwitterProviderForRepliesToUserForChannel,
+  SaveDatToTwitterProviderForMentionedUserForChannel: SaveDatToTwitterProviderForMentionedUserForChannel,
+  TweetsScrapper: TweetsScrapper
+};
+
+
+function SaveDatToTwitterProviderForRepliesToUserForChannel(since, until, channelId, campaignId, keywords, mentionedUser) {
+
+  var since = since;
+  var until = until;
+  var mentionedUser = mentionedUser;
+  var keywords = keywords;
+
+  var keywordsArrayLength = keywords.length;
+  var finalKeywords = null;
+
+  var channelId = channelId;
+  var campaignId = campaignId;
+
+  for (var i = 0; i < keywordsArrayLength; i++) {
+    if (finalKeywords == null) {
+      finalKeywords = ""
+    }
+    if (keywords.indexOf(keywords[i]) != keywordsArrayLength - 1) {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" OR ';
+    }
+    else {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" ';
+    }
+  }
+
+
+  console.log(finalKeywords);
+
+
+  function scrap(max_id) {
+
+    // return new Promise(function (resolve, reject) {
+
+
+    client.get('search/tweets', {
+      q: 'to:' + mentionedUser + ' ' + finalKeywords + ' since:' + since + ' until:' + until + 'result_type:popular',
+      count: 100,
+      max_id: max_id
+    }, function (error, tweets, response) {
+      if (error) {
+        return -1;
+      }
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        var newTwitterData = new dataProvider.tweetsProvider();
+        newTwitterData.id = tweets.statuses[i].id_str;
+        newTwitterData.dateContent = tweets.statuses[i].created_at;
+        var newContent = tweets.statuses[i].text.replace(/\r?\n|\r/g, " ");
+        newTwitterData.content = newContent;
+        newTwitterData.contentLanguage = tweets.statuses[i].lang;
+        newTwitterData.author = {
+          screenName: tweets.statuses[i].user.screen_name,
+          id: tweets.statuses[i].user.screen_name
+        }; //
+        newTwitterData.dateOfScraping = new Date();
+        newTwitterData.hashtags = tweets.statuses[i].entities.hashtags; //
+        newTwitterData.tweetType = "Reply";
+        newTwitterData.resultType = tweets.statuses[i].metadata.result_type;
+        newTwitterData.counts = {
+          retweets: tweets.statuses[i].retweet_count,
+          favorites: tweets.statuses[i].favorite_count
+        }
+        newTwitterData.channelId = channelId;
+        newTwitterData.campaignId = campaignId;
+
+        dataProvider.createDataProviderModel(newTwitterData, function (err, data) {
+          if (err)
+            console.log('Error', err);
+          else {
+
+            //console.log('Saved Successfully: ', data);
+
+          }
+        });
+      }
+
+
+      if (tweets.search_metadata.next_results == null) {
+        //res.status(200).json({"End":tweets})
+        console.log('End Of Scrapping For Replies')
+
+      }
+      else {
+        var max = tweets.search_metadata.next_results.slice(8, 26);
+        console.log(max);
+        if (max) {
+          count1++;
+          console.log('countRep: ', count1)
+          scrap(max);
+        }
+        else {
+          console.log('ElsecountRep: ', count1)
+        }
+
+        //
+      }
+
+    });
+
+  }
+
+  scrap(null);
+};
+
+
+function SaveDatToTwitterProviderForMentionedUserForChannel(since, until, channelId, campaignId, keywords, mentionedUser) {
+
+  var since = since;
+  var until = until;
+  var mentionedUser = mentionedUser;
+  var keywords = keywords;
+
+  var keywordsArrayLength = keywords.length;
+  var finalKeywords = null;
+
+  var channelId = channelId;
+  var campaignId = campaignId;
+
+  for (var i = 0; i < keywordsArrayLength; i++) {
+    if (finalKeywords == null) {
+      finalKeywords = ""
+    }
+    if (keywords.indexOf(keywords[i]) != keywordsArrayLength - 1) {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" OR ';
+    }
+    else {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" ';
+    }
+  }
+
+
+  console.log(finalKeywords);
+
+
+  function scrap(max_id) {
+
+    // return new Promise(function (resolve, reject) {
+
+
+    client.get('search/tweets', {
+      q: '@' + mentionedUser + ' ' + finalKeywords + ' since:' + since + ' until:' + until + 'result_type:popular',
+      count: 100,
+      max_id: max_id
+    }, function (error, tweets, response) {
+      if (error) {
+        return -1;
+      }
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        var newTwitterData = new dataProvider.tweetsProvider();
+        newTwitterData.id = tweets.statuses[i].id_str;
+        newTwitterData.dateContent = tweets.statuses[i].created_at;
+        var newContent = tweets.statuses[i].text.replace(/\r?\n|\r/g, " ");
+        newTwitterData.content = newContent;
+        newTwitterData.contentLanguage = tweets.statuses[i].lang;
+        newTwitterData.author = {
+          screenName: tweets.statuses[i].user.screen_name,
+          id: tweets.statuses[i].user.screen_name
+        }; //
+        newTwitterData.dateOfScraping = new Date();
+        newTwitterData.hashtags = tweets.statuses[i].entities.hashtags; //
+        newTwitterData.tweetType = "Mention";
+        newTwitterData.resultType = tweets.statuses[i].metadata.result_type;
+        newTwitterData.counts = {
+          retweets: tweets.statuses[i].retweet_count,
+          favorites: tweets.statuses[i].favorite_count
+        }
+        newTwitterData.channelId = channelId;
+        newTwitterData.campaignId = campaignId;
+
+        dataProvider.createDataProviderModel(newTwitterData, function (err, data) {
+          if (err)
+            console.log('Error', err);
+          else {
+
+            //console.log('Saved Successfully: ', data);
+
+          }
+        });
+      }
+
+
+      if (tweets.search_metadata.next_results == null) {
+        //res.status(200).json({"End":tweets})
+        console.log('End Of Scrapping For Mentions')
+      }
+      else {
+        var max = tweets.search_metadata.next_results.slice(8, 26);
+        console.log(max);
+        if (max) {
+          count2++;
+          console.log('countMent: ', count2)
+          scrap(max);
+        }
+        else {
+          console.log('ElsecountMent: ', count2)
+        }
+
+        //
+      }
+
+    });
+
+  }
+
+  scrap(null);
+
+
+};
+
+
+function TweetsScrapper(since, until, channelId, campaignId, keywords) {
+
+  var since = since;
+  var until = until;
+  var keywords = keywords;
+
+  var keywordsArrayLength = keywords.length;
+  var finalKeywords = null;
+
+  var channelId = channelId;
+  var campaignId = campaignId;
+
+  for (var i = 0; i < keywordsArrayLength; i++) {
+    if (finalKeywords == null) {
+      finalKeywords = ""
+    }
+    if (keywords.indexOf(keywords[i]) != keywordsArrayLength - 1) {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" OR ';
+    }
+    else {
+      finalKeywords = finalKeywords + '"' + keywords[i] + '" ';
+    }
+  }
+
+
+  console.log(finalKeywords);
+  function scrap(max_id) {
+
+    client.get('search/tweets', {
+      q: '' + finalKeywords + ' since:' + since + ' until:' + until + '',
+      count: 100, max_id: max_id
+    }, function (error, tweets, response) {
+      if (error) {
+        return -1;
+      }
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        var newTwitterData = new dataProvider.tweetsProvider();
+        newTwitterData.id = tweets.statuses[i].id_str;
+        newTwitterData.dateContent = tweets.statuses[i].created_at;
+        var newContent = tweets.statuses[i].text.replace(/\r?\n|\r/g, " ");
+        newTwitterData.content = newContent;
+        newTwitterData.contentLanguage = tweets.statuses[i].lang;
+        newTwitterData.author = {
+          screenName: tweets.statuses[i].user.screen_name,
+          id: tweets.statuses[i].user.screen_name
+        }; //
+        newTwitterData.dateOfScraping = new Date();
+        newTwitterData.hashtags = tweets.statuses[i].entities.hashtags; //
+        newTwitterData.tweetType = "NormalScraper";
+        newTwitterData.resultType = tweets.statuses[i].metadata.result_type;
+        newTwitterData.counts = {
+          retweets: tweets.statuses[i].retweet_count,
+          favorites: tweets.statuses[i].favorite_count
+        }
+        newTwitterData.channelId = channelId;
+        newTwitterData.campaignId = campaignId;
+
+        dataProvider.createDataProviderModel(newTwitterData, function (err, data) {
+          if (err)
+            console.log('Error', err);
+          else {
+
+            //console.log('Saved Successfully: ', data);
+          }
+        });
+      }
+
+      if (tweets.search_metadata.next_results == null) {
+        //res.status(200).json({"End":tweets})
+        console.log('End Of Scrapping')
+      }
+      else {
+        var max = tweets.search_metadata.next_results.slice(8, 26);
+        console.log(max);
+        if (max) {
+          count3++;
+          console.log('countAll: ', count3)
+          scrap(max);
+        }
+        else {
+          console.log('ElsecountAll: ', count3)
+        }
+
+        //
+      }
+
+
+    });
+  }
+
+  scrap(null);
+
+};
--- server/routes/API/twitter/twitterCron.js
+++ server/routes/API/twitter/twitterCron.js
@@ -6,65 +6,82 @@ var DataProvider = require('../../../models/dataProvider/dataProvider.model');
 var CampaignModel = require('../../../models/campaign.model');
 var Channel = require('../../../models/channel/channel.model');
 var async = require('async');
+var TwitterAPIFunctions = require('./twitterAPIFunctions');
+var moment = require('moment');
+
+var today = moment(new Date()).format('YYYY-MM-DD')
+
+var yesterday = moment(new Date(new Date().setDate(new Date().getDate() - 1))).format('YYYY-MM-DD');
+
 
 module.exports.run = function (req, res) {
-var task = cron.schedule('* * * * * *', function(){
+  var task = cron.schedule('* * * * *', function () {
 
-  var websitesAndKeywords = [];
+    var websitesAndKeywords = [];
 
 
-
-  CampaignModel.findAllCampaigns().then(function (campaigns) {
-    var channelPromise;
-    async.eachSeries(campaigns, function iteratee(campaign, callback) {
-      campaign.channels.forEach(function (campaignChannel) {
-        channelPromise = new Promise(function (resolve, reject) {
-          Channel.getChannelByIdModel(campaignChannel.channelId, function (err, channelById) {
-            if (!channelById) {
-              console.log("nooo");
-              resolve(channelById);
-            }
-            else {
-              console.log("heyy");
-              if (channelById.type === "twitter") {
-                websitesAndKeywords.push({
-                  "campaignId": campaign._id,
-                  "channelId": channelById._id,
-                  "url": channelById.url,
-                  "keywords": campaign.keywords
-                });
-                console.log(websitesAndKeywords);
+    CampaignModel.findAllNewCreatedCampaigns().then(function (campaigns) {
+      var channelPromise;
+      async.eachSeries(campaigns, function iteratee(campaign, callback) {
+        campaign.channels.forEach(function (campaignChannel) {
+          channelPromise = new Promise(function (resolve, reject) {
+            Channel.getChannelByIdModel(campaignChannel.channelId, function (err, channelById) {
+              if (!channelById) {
+                console.log("Error");
+                resolve(channelById);
               }
-              resolve(channelById);
-            }
-          });
+              else {
+                console.log("Entring to else");
+                if (channelById.type === "twitter") {
+                  var kyTab = []
+
+
+                  campaign.keywords.forEach(function (item) {
+                    kyTab.push(item.content);
+                  });
+
+                  var pathArray = channelById.url.split('/');
+                  var ScreenName = pathArray[3];
+
+                  websitesAndKeywords.push({
+                    "since": yesterday,
+                    "until": today,
+                    "campaignId": campaign._id,
+                    "channelId": channelById._id,
+                    "url": channelById.url,
+                    "keywords": kyTab,
+                    "screenName": ScreenName
+                  });
+                }
+                resolve(channelById);
+              }
+            });
+          })
+        });
+        channelPromise.then(function (data) {
+          callback()
         })
+      }, function done() {
+        console.log("THE END OF ANALYSING");
+
+        console.log("websitesAndKeywords: ", websitesAndKeywords);
+
+// i will add if startdate < date of creation, then scrap all data from creation date to today, else scrap only for yesterday.
+        websitesAndKeywords.forEach(function (item) {
+          TwitterAPIFunctions.SaveDatToTwitterProviderForRepliesToUserForChannel(item.since, item.until, item.channelId, item.campaignId, item.keywords, item.screenName)
+          TwitterAPIFunctions.SaveDatToTwitterProviderForMentionedUserForChannel(item.since, item.until, item.channelId, item.campaignId, item.keywords, item.screenName)
+          TwitterAPIFunctions.TweetsScrapper(item.since, item.until, item.channelId, item.campaignId, item.keywords)
+
+        })
+
+
       });
-      channelPromise.then(function (data) {
-        callback()
-      })
-    }, function done() {
-      console.log("end");
-
-
+    }).catch(function (err) {
     });
-  }).catch(function (err) {
+
+
   });
-
-
-
-
-
-
-
-
-
-
-
-});
-task.start();
-
-
+  task.start();
 
 
 };
